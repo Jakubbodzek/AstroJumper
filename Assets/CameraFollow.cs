309efb6a3a503f02d0c72968287371f2c71e417a
Jakub Bodzek <jakubbodzek03@gmail.com>
2021-12-12 19:08:47 +0100

    Added Player Movement Added Game and Menu Scenes Added Player Animations

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+
+    [SerializeField] private Vector3 offset;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private Transform groundPos;
+
+    [SerializeField] private float checkRadius;
+
+    [SerializeField] private LayerMask whatIsGround;
+
+    [SerializeField] private float jumpTime;
+
+
+    private bool isGrounded;
+    private Rigidbody2D rigidBody;
+    private float jumpTimeCounter;
+    private bool isJumping;
+    private bool doubleJump;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Takeoff");
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rigidBody.velocity = Vector2.up * jumpForce;
+        }
+
+        if(isGrounded == true)
+        {
+            doubleJump = false;
+            animator.SetBool("IsJumping", false);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rigidBody.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+        if (isGrounded == false && doubleJump == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            doubleJump = true;
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rigidBody.velocity = Vector2.up * jumpForce;
+        }
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(moveInput * speed, rigidBody.velocity.y);
+
+        if (moveInput == 0)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
+        if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+
+
+}

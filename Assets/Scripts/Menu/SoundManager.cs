4714573c9454d5a5940f3a46cfef5a1f22e01eb2
Jakub Bodzek <jakubbodzek03@gmail.com>
2021-12-19 22:44:23 +0100

    Small changes to sound manager and MenuView

--- Assets/Scripts/Menu/SoundManager.cs
+++ Assets/Scripts/Menu/SoundManager.cs
@@ -5,17 +5,20 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    // Sound Manager is strictly for music used in the game
 
-
+    #region Components
     [SerializeField] private Image musicOn;
 
     [SerializeField] private Image musicOff;
 
-    private bool muted = false;
+    private bool muted = false; 
+    #endregion
 
+    #region Start
     void Start()
     {
-        if(!PlayerPrefs.HasKey("muted"))
+        if (!PlayerPrefs.HasKey("muted"))
         {
             PlayerPrefs.SetInt("muted", 0);
             Load();
@@ -26,11 +29,13 @@ public class SoundManager : MonoBehaviour
         }
         UpdateButtonIcon();
         AudioListener.pause = muted;
-    }
+    } 
+    #endregion
 
+    #region Icon Press
     public void OnButtonPress()
     {
-        if(muted == false)
+        if (muted == false)
         {
             muted = true;
             AudioListener.pause = true;
@@ -43,11 +48,13 @@ public class SoundManager : MonoBehaviour
 
         Save();
         UpdateButtonIcon();
-    }
+    } 
+    #endregion
 
+    #region Icon Update
     private void UpdateButtonIcon()
     {
-        if(muted == false)
+        if (muted == false)
         {
             musicOn.enabled = true;
             musicOff.enabled = false;
@@ -58,15 +65,20 @@ public class SoundManager : MonoBehaviour
             musicOn.enabled = false;
             musicOff.enabled = true;
         }
-    }
+    } 
+    #endregion
 
+    #region Load
     private void Load()
     {
         muted = PlayerPrefs.GetInt("muted") == 1;
-    }
+    } 
+    #endregion
 
+    #region Save
     private void Save()
     {
         PlayerPrefs.SetInt("muted", muted ? 1 : 0);
-    }
+    } 
+    #endregion
 }
